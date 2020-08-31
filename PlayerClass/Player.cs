0f4286435635a21de8c0e110ad24f4057c436e52
whit <whit@stroups.net>
2020-08-31 14:16:51 -0500

    Created Player Class Exmaple for class

---
+++ PlayerClass/Player.cs
@@ -0,0 +1,48 @@
+﻿using System;
+namespace PlayerClass
+{
+    public class Player
+    {
+        public Player()
+        {
+
+        }
+
+        public Player(string name, string weapon, int attackValue, string species)
+        {
+            Name = name;
+            Weapon = weapon;
+            AttackValue = attackValue;
+            Species = species;
+        }
+
+
+
+        private int health = 1000;
+
+        public string Name { get; set; }
+        public string Weapon { get; set; }
+        public int AttackValue { get; set; }
+        public string Species { get; set; }
+
+        public void RecieveDamage(int damageAmount)
+        {
+            health -= damageAmount;
+        }
+
+        public void Attack()
+        {
+            Console.WriteLine($"{Name}: attacking with my {Weapon}!");
+        }
+
+        public void Speak()
+        {
+            Console.WriteLine($"{Name}: speaking in language of {Species}: Hello!");
+        }
+
+        public void SayHealth()
+        {
+            Console.WriteLine($"{Name}'s health is {health}");
+        }
+    }
+}
---
+++ PlayerClass/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace PlayerClass
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            var playerThree = new Player("Jeremiah", "Katana", 700, "Human");
+
+            var playerOne = new Player();
+            playerOne.AttackValue = 5;
+            playerOne.Weapon = "Garden Tool";
+            playerOne.Species = "Hobbit";
+            playerOne.Name = "Jonny";
+            
+
+            var playerTwo = new Player()
+            {
+                AttackValue = 100,
+                Name = "Bubba",
+                Weapon = "Club",
+                Species = "Ogre"
+            };
+
+            Console.WriteLine(playerThree.Species);
+
+            playerOne.Speak();
+            playerTwo.Speak();
+
+            playerOne.Attack();
+
+            playerTwo.RecieveDamage(playerOne.AttackValue);
+
+            playerTwo.SayHealth();
+        }
+    }
+}
